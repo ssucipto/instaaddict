de2b53a8d7ce42c21821e425dc38d0f4582dd507
joeahkim <joeahkim@gmail.com>
2026-07-17 16:57:55 +0300

    First version

--- GramAddict/__init__.py
+++ GramAddict/__init__.py
@@ -1,6 +1,6 @@
 """InstaAddict - Human-like Instagram bot powered by UIAutomator2"""
 
-__version__ = "3.2.12"
+__version__ = "1.0.1"
 __tested_ig_version__ = "438.0.0.28.88"
 
 from GramAddict.core.bot_flow import start_bot
