b7c85f97da11b1bde44c7127d9202ea77b506742
joeahkim <joeahkim@gmail.com>
2026-07-17 16:26:56 +0300

    Update to version 438.0.0.28.88

--- GramAddict/__init__.py
+++ GramAddict/__init__.py
@@ -1,7 +1,7 @@
 """Human-like Instagram bot powered by UIAutomator2"""
 
 __version__ = "3.2.12"
-__tested_ig_version__ = "300.0.0.29.110"
+__tested_ig_version__ = "438.0.0.28.88"
 
 from GramAddict.core.bot_flow import start_bot
 
